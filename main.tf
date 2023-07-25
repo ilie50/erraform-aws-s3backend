e41a18d4e29ae8e26052fa14c26a20b543ac8113
ilie50 <ilie50@yahoo.com>
2023-07-25 15:59:11 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,91 @@
+
+provider "aws" {
+  region = "eu-west-2"
+  profile = "ilieb"
+}
+
+data "aws_region" "current" {}
+
+resource "random_string" "rand" {
+  length  = 24
+  special = false
+  upper   = false
+}
+
+locals {
+  namespace = substr(join("-", [var.namespace, random_string.rand.result]), 0, 24)
+}
+
+resource "aws_resourcegroups_group" "resourcegroups_group" {
+  name = "${local.namespace}-group"
+
+  resource_query {
+    query = <<-JSON
+{
+  "ResourceTypeFilters": [
+    "AWS::AllSupported"
+  ],
+  "TagFilters": [
+    {
+      "Key": "ResourceGroup",
+      "Values": ["${local.namespace}"]
+    }
+  ]
+}
+  JSON
+  }
+}
+
+resource "aws_kms_key" "kms_key" {
+  tags = {
+    ResourceGroup = local.namespace
+  }
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket        = "${local.namespace}-state-bucket"
+  force_destroy = var.force_destroy_state
+
+  tags = {
+    ResourceGroup = local.namespace
+  }
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+bucket  = aws_s3_bucket.this.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.kms_key.arn
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "dynamodb_table" {
+  name         = "${local.namespace}-state-lock"
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  tags = {
+    ResourceGroup = local.namespace
+  }
+}
